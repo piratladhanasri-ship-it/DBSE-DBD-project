05d5e4dac459b282fcfd617920dc16e90c757dbd
piratladhanasri-ship-it <piratladhanasri@klh.edu.in>
2026-09-19 08:20:34 +0530

    Delete src directory

--- src/utils/cn.js
+++
@@ -1,6 +0,0 @@
-import { twMerge } from 'tailwind-merge';
-
-/** Joins conditional class names and resolves Tailwind conflicts. */
-export function cn(...inputs) {
-  return twMerge(inputs.filter(Boolean).join(' '));
-}
